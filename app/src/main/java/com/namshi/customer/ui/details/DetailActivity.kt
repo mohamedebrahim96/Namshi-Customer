1dd98b5cd9859d081379575eda530a89d4a78717
Mohamed Ebrahim <ebrahimm131@gmail.com>
2021-11-21 12:48:20 +0400

    add unit testing libs and write injection instrumation test

--- app/src/main/java/com/namshi/customer/ui/details/DetailActivity.kt
+++ app/src/main/java/com/namshi/customer/ui/details/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.namshi.customer.ui.details
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.VisibleForTesting
 import com.namshi.customer.R
 import com.namshi.customer.databinding.ActivityDetailBinding
 import com.skydoves.bindables.BindingActivity
@@ -15,6 +17,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+
+    @get:VisibleForTesting
+    internal val viewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
